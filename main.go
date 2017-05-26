f719adb8107407c1ca15460b817eb537f8edd1cf
steffey <steven.d.steffey@wmich.edu>
2017-05-25 23:17:19 -0400

    Now parsing commands in a more logical manner through the use of a switch rather than nested ifs. I've also added the office command, a joke spy command to make light of recent events, and a placeholder for the help command. Bot can now output to public or private channels.

--- main.go
+++ main.go
@@ -3,23 +3,42 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 )
 
 func main() {
 	// start Real Time API session
-	ws, id := slackConnect()
+	slackSock, botID := slackConnect()
 	fmt.Println("Connected")
-	fmt.Println(id)
+	fmt.Println(botID)
 
 	for {
 		// read each incoming message
-		m, err := getMessage(ws)
-		if m.Type == "message" {
-			fmt.Printf("msg")
-			pollOffice()
-		}
+		message, err := getMessage(slackSock)
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("error retrieving message: %s", err)
 		}
+
+		if message.Type == "message" && strings.HasPrefix(message.Text, "<@"+botID+">") {
+			//clean up text by removing the prefix
+			message.Text = strings.Trim(message.Text, "<@"+botID+"> ")
+
+			//command block
+			switch message.Text {
+			case "office":
+				unregCount, regList := pollOffice()
+				message.Text = fmt.Sprintf("Unregistered: %d, Registered: %s", unregCount, regList[0])
+				postMessage(slackSock, message)
+			case "spy":
+				message.Text = "I'm already doing that."
+				postMessage(slackSock, message)
+			default:
+				message.Text = "what are you doin'?"
+				postMessage(slackSock, message)
+			}
+
+		}
+
 	}
+
 }
